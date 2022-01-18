ffb11076cceef85e16d6c4cde6dfd53dec3e7efe
ashok-tb <ashok.chaudhary@testbook.com>
2022-01-18 13:22:23 +0530

    Fixed home routing issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MoviesComponent } from './movies/movies.component';
 import { SearchboxComponent } from './searchbox/searchbox.component';
-import { PaginationButtonComponent } from './pagination-button/pagination-button.component';
 const routes: Routes = [
   {path:'search/:sTerm/:page', component:MoviesComponent},
 
